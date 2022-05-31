e08612af78fe54b891af211445c0e98c0845523f
Chimeratech-py <xkenntsx619@gmail.com>
2022-05-31 18:19:21 +0800

    Before switch to new laptop

--- client/src/app/_components/profile/profile.component.ts
+++ client/src/app/_components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Profile } from 'src/app/_models/profile';
+import { ProfileService } from 'src/app/_services/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  localProfile!: Profile;
+  
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
+    this.getKenneth();
+  }
+
+  getKenneth(){
+    return this.profileService.getProfile().subscribe({
+      next: response => this.localProfile = response,
+      error: error => console.log(error)
+    })
   }
 
 }
